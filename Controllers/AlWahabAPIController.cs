5b9a63bd01de8202fcf82cd6c0bfc34d82693406
tasawarahmed <tasawarahmed@yahoo.com>
2024-02-08 16:34:32 +0500

    Updated controller for list of students, result, fee, and unpaid fee

--- Controllers/AlWahabAPIController.cs
+++ Controllers/AlWahabAPIController.cs
@@ -1,5 +1,8 @@
 ﻿using AlWahabAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -23,11 +26,39 @@ namespace AlWahabAPI.Controllers
             return _dbContext.ViewStudents;
         }
 
-        // GET api/<AlWahabAPIController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET api/<AlWahabAPIController>/Students/5
+        [HttpGet("Students/{id}")]
+        public async Task<ActionResult<IEnumerable<ViewStudent>>>  Get(string id)
         {
-            return "value";
+            var result = await _dbContext.ViewStudents.FromSqlRaw($"GetStudentsByCNIC {id}").ToListAsync();
+            return Ok(result);
+        }
+
+        // GET api/<AlWahabAPIController>/Results/5
+        [HttpGet("Results/{id}")]
+        public async Task<ActionResult<IEnumerable<AlWahabAPI.Models.ViewResult>>> GetStudentResult(int id)
+        {
+            var result = await _dbContext.ViewResults.FromSqlRaw("EXEC GetStudentResultByID @stuID", new SqlParameter("@stuID", id))
+                .ToListAsync();
+            return Ok(result);
+        }
+
+        // GET api/<AlWahabAPIController>/PaidFee/5
+        [HttpGet("PaidFee/{id}")]
+        public async Task<ActionResult<IEnumerable<AlWahabAPI.Models.ViewFeePaidApi>>> GetStudentPaidFee(long id)
+        {
+            var result = await _dbContext.ViewFeePaidApis.FromSqlRaw("EXEC GetPaidFeeByID @stuID", new SqlParameter("@stuID", id))
+                .ToListAsync();
+            return Ok(result);
+        }
+
+        // GET api/<AlWahabAPIController>/UnPaidFee/5
+        [HttpGet("UnPaidFee/{id}")]
+        public async Task<ActionResult<IEnumerable<AlWahabAPI.Models.ViewFeeReceivableApi>>> GetStudentUnPaidFee(long id)
+        {
+            var result = await _dbContext.ViewFeeReceivableApis.FromSqlRaw("EXEC GetUnPaidFeeByID @stuID", new SqlParameter("@stuID", id))
+                .ToListAsync();
+            return Ok(result);
         }
     }
 }
